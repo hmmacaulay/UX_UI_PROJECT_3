1df36bdbd81543cd9374d89211746e33dd8777f4
hmmacaulay <66646061+hmmacaulay@users.noreply.github.com>
2021-01-26 16:27:09 -0800

    pushing general updates, starting styling

---
+++ js/etc.js
@@ -0,0 +1,11 @@
+// $(function() {
+//     $('.nav_bar').load('../nav.html')
+// });
+// $(document).ready(function() {
+//     // var nav_bar = $(nav_bar).html("")
+//     $("#nav_bar").load("../nav.html")  
+// });
+
+$(document).ready(function() {
+    $("#nav_bar").load("../nav.html #hi");
+});
